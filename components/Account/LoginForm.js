66e21bab5e1edf0bd1f506c8dcfe155a7609b824
Tuan Anh <vuhoangtuananh30041994@gmail.com>
2022-12-10 12:08:46 -0800

    update

--- components/Account/LoginForm.js
+++ components/Account/LoginForm.js
@@ -30,7 +30,7 @@ function LoginForm() {
       <input
         type="text"
         className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-        placeholder="Bussiness Name"
+        placeholder="Bussiness Email"
         onChange={(e)=>setEmail(e.target.value)}
       />
     </div>
